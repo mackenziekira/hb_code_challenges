a7dabe9bd31a1d82bc17fdff4e8b2a4bba4d4610
vagrant <vagrant@vagrant.vm>
2016-12-11 20:29:10 +0000

    lazy lemmings

---
+++ bst-add-child/bstAddChild.py
@@ -0,0 +1,37 @@
+"""
+    >>> t = Node(4,
+    ...       Node(2, Node(1), Node(3)),
+    ...       Node(6, Node(5), Node(7))
+    ... )
+
+    >>> t.add_child(0)
+    >>> t.left.left.left.data
+    0
+
+    >>> t.add_child(4)
+    >>> t.right.left.left.data
+    4
+
+    >>> t.add_child(6)
+    >>> t.right.right.left.data
+    6
+"""
+
+class Node(object):
+    """Binary search tree node."""
+
+    def __init__(self, data, left=None, right=None):
+        """Create node, with data and optional left/right."""
+
+        self.left = left
+        self.right = right
+        self.data = data
+
+    def add_child(self):
+        
+
+if __name__ == "__main__":
+    import doctest
+    
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. NODES ADDED SUCCESSFULLY!\n"
