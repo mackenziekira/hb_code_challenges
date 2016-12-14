962220356c2f1df627e2be330886f2ae7f65cf69
vagrant <vagrant@vagrant.vm>
2016-12-14 21:20:45 +0000

    working on count employees

---
+++ pangram/pangram.py
@@ -0,0 +1,25 @@
+"""Given a string, return True if it is a pangram, False otherwise.
+
+For example::
+
+    >>> is_pangram("The quick brown fox jumps over the lazy dog!")
+    True
+    
+    >>> is_pangram("I love cats, but not mice")
+    False
+"""
+
+
+def is_pangram(sentence):
+    """Given a string, return True if it is a pangram, False otherwise."""
+
+    letters = set()
+
+
+
+
+
+if __name__ == "__main__":
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. YAY!\n"
