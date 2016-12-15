f002965f5100d48c62c36a7d17201976bec95ef0
vagrant <vagrant@vagrant.vm>
2016-12-15 22:00:55 +0000

    better pangram solution

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -13,13 +13,16 @@ For example::
 def is_pangram(sentence):
     """Given a string, return True if it is a pangram, False otherwise."""
 
-    letters = set(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','v','w','x','y','z'])
+    # letters = set(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','v','w','x','y','z'])
 
-    for letter in sentence:
-        if letter in letters:
-            letters.remove(letter)
+    # for letter in sentence:
+    #     if letter in letters:
+    #         letters.remove(letter)
 
-    return not letters
+    # return not letters
+
+    letters = {char.lower() for char in sentence if char.isalpha()}
+    return len(letters) == 26
 
 
 
