5e824c040b6dcfd586fb8e1b8f5fe4499409cfe3
Mackenzie Dolginow <mackenziekira@gmail.com>
2017-01-13 14:40:11 -0800

    find nth from last element in linked list

---
+++ nth_from_last_ll.py
@@ -0,0 +1,49 @@
+class Node(object):
+    """node class
+
+    >>> print Node(3, Node(5))
+    3
+    """
+
+    def __init__(self, data = None, next = None):
+        self.data = data
+        self.next = next
+
+    def __repr__(self):
+        return str(self.data)
+
+class LinkedList(object):
+    """linkedlist class"""
+
+    def __init__(self, head):
+        self.head = head
+
+    def nth_from_last_ll(self, n):
+        """returns nth from last node in linked list
+        """
+
+        first_runner = self.head
+        second_runner = None
+        i = 0
+        while first_runner.data is not None:
+            if i == n + 1:
+                second_runner = head
+            first_runner = first_runner.next
+            if second_runner:
+                second_runner = second_runner.next
+        return second_runner
+
+n1 = Node(4)
+n2 = Node(5)
+n3 = Node(6)
+
+n1.next = n2
+n2.next = n3
+
+ll = LinkedList(n1)
+ll.nth_from_last_ll(2)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
