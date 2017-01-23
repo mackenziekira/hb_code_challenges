21800715c60f10088efb7f20e54474f0fe4fc747
Mackenzie Dolginow <mackenziekira@gmail.com>
2017-01-23 12:24:02 -0800

    changed dec to bin to be dec to any base, 26 and under

--- dec2bin/dec2bin.py
+++ dec2bin/dec2bin.py
@@ -37,27 +37,30 @@ For example, using our alternate solution::
 """
 from collections import deque
 
-def dec2bin(num):
+def dec2bin(num, base):
     """Convert a decimal number to binary representation."""
+    nums = '0123456789abcdefghijklmnop'
 
     stack = deque()
 
     while num > 0:
-        remainder = num % 2
+        remainder = num % base
         stack.append(remainder)
-        num = num / 2
+        num = num / base
 
     result = ''
     while stack.__len__() > 0:
-        result += str(stack.pop())
+        result += nums[stack.pop()]
 
     if not result:
         return '0'
     return result
 
+print dec2bin(26, 26)
 
 
-if __name__ == '__main__':
-    import doctest
-    if doctest.testmod().failed == 0:
-        print "\n*** ALL TEST PASSED. W00T!\n"
+
+# if __name__ == '__main__':
+#     import doctest
+#     if doctest.testmod().failed == 0:
+#         print "\n*** ALL TEST PASSED. W00T!\n"
