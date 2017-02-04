dbe13fc150318cd2c7a5819d90b50f2a226ccb2b
Mackenzie Dolginow <mackenziekira@gmail.com>
2017-02-03 16:00:18 -0800

    implementing quicksort. set up doctest

---
+++ quicksort.py
@@ -0,0 +1,10 @@
+def quicksort(lst):
+    """
+    >>> quicksort([5, 7, 3, 9, 10, 2, 1])
+    [1, 2, 3, 5, 7, 9, 10]
+    """
+    
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
